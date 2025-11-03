b7a9bf713762d0ebff8a2f9cf8ab11fe573bba36
Arani Bhaskararao <bhaskara.ee@gmail.com>
2025-11-03 18:14:50 +0000

    Fixed build issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "./Logo";
 
